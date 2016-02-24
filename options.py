de817727e32884526da6680d9c1a18c2da405abd
Al Carruth <carruth@volente.com>
2016-02-23 18:07:18 -0600

    modified:   options.py 	new file:   tickets.db

--- options.py
+++ options.py
@@ -6,10 +6,10 @@ db_name = 'tickets'
 
 # db backend 
 # either 'postgres' or 'sqlite' 
-engine_type = 'postgres'
+engine_type = 'sqlite'
 
 # options for dummy data creation
 year = 2016
 number_of_games = 11
 number_of_users = 1000
-number_of_ticket_lots = 10000
+number_of_ticket_lots = 5000
