b87af9baebb72e7fb68468ff0581f16961fe5263
Al Carruth <carruth@volente.com>
2016-03-12 13:31:43 -0600

    modified:   requirements.py

--- requirements.py
+++ requirements.py
@@ -5,16 +5,16 @@ import sys
 
 fmt_str = '%s: %s'
 
+# dict2xml has no version information available
 deps = {
     'oauth2client': '__version__',
     'werkzeug': '__version__',
-    'dict2xml': '',
+    'dict2xml': '__name__',
     'flask': '__version__',
     'sqlalchemy': '__version__',
     'sqlite3': 'version', 
     'psycopg2': '__version__'
 } 
- 
 
 def check_dep(mod):
     try:
@@ -23,9 +23,7 @@ def check_dep(mod):
     except:
         print '%s not found' % mod 
 
-
 if __name__ == '__main__':
-
 
     print fmt_str % ('python', str.split(sys.version, ' ')[0])
     print fmt_str % ('postgresql', str.split(check_output(['psql', '--version']).strip(),' ')[2])
